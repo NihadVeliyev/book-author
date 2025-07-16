d7b37937ba3e3f6937a33bef56b307b8c29f2240
NihadVeliyev <nihadv795@gmail.com>
2025-07-16 14:35:22 +0400

    Add AuthorController with endpoints to retrieve all authors and find an author by ID

--- src/main/java/az/edu/turing/bookauthor/controller/AuthorController.java
+++ src/main/java/az/edu/turing/bookauthor/controller/AuthorController.java
@@ -1,10 +1,36 @@
 package az.edu.turing.bookauthor.controller;
 
+import az.edu.turing.bookauthor.model.Author;
+import az.edu.turing.bookauthor.service.AuthorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class AuthorController {
+
+    public final AuthorService authorService;
+
+    @GetMapping("/authors")
+    public List<Author> getAllAuthors(){
+        return authorService.getAllAuthors();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Author> findById(@PathVariable Long id){
+        return authorService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+
+    }
+    
+
 
 }
