6cff44bc34d3dfc02b111f35752aaa488dd948b6
NihadVeliyev <nihadv795@gmail.com>
2025-07-16 23:49:51 +0400

    Add findById and createBook endpoints to BookController

--- src/main/java/az/edu/turing/bookauthor/controller/BookController.java
+++ src/main/java/az/edu/turing/bookauthor/controller/BookController.java
@@ -4,10 +4,8 @@ package az.edu.turing.bookauthor.controller;
 import az.edu.turing.bookauthor.model.Book;
 import az.edu.turing.bookauthor.service.BookService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +21,20 @@ public class BookController {
     }
 
     @GetMapping("/books/{id}")
+    public ResponseEntity<Book> findById(@PathVariable Long id){
+        return bookService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+
+    }
+
+    @PostMapping("/books")
+    public ResponseEntity<Book> createBook(@RequestBody Book book){
+        Book createdBook=bookService.createBook(book);
+        return ResponseEntity.status(201).body(createdBook);
+
+
+    }
 
 
 
